7b72e45b7e72da2224540b640593964ab6218f6c
Pedro <rpedroricardo87@gmail.com>
2026-04-02 16:44:09 -0300

    fix: consertando teste

--- src/square.test.ts
+++ src/square.test.ts
@@ -1,9 +1,9 @@
 import {quadrado, quadrado2} from "./square";
 
 test('Tranforme [3,5,7,3,8,9,1] to [9,25,49,9,64,81,1]', () => {
-  expect(quadrado([3,5,7,3,8,9,1])).toBe([9,25,49,9,64,81,1]);
+  expect(quadrado([3,5,7,3,8,9,1])).toStrictEqual([9,25,49,9,64,81,1]);
 });
 
 test('Tranforme [3,5,7,3,8,9,1] to [9,25,49,9,64,81,1] with forEach', () => {
-  expect(quadrado2([3,5,7,3,8,9,1])).toBe([9,25,49,9,64,81,1]);
+  expect(quadrado2([3,5,7,3,8,9,1])).toStrictEqual([9,25,49,9,64,81,1]);
 });
