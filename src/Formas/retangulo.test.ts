48bf487cfcd31b686dccbaeee786032cc436d13c
Pedro <rpedroricardo87@gmail.com>
2026-04-02 22:29:39 -0300

    fix: arrumando classe retangulo

--- src/Formas/retangulo.test.ts
+++ src/Formas/retangulo.test.ts
@@ -1,9 +1,11 @@
-import {retangulo} from "./retangulo";
+import {Retangulo} from "./retangulo";
 
 test('Calculate the area of a rectangle with base 5 and height 3', () => {
-  expect(retangulo(5, 3)).toBe(15);
+  const retangulo = new Retangulo(5, 3);
+  expect(retangulo.calcularArea()).toBe(15);
 });
 
 test('Calculate the perimeter of a rectangle with base 5 and height 3', () => {
-  expect(retangulo(5, 3)).toBe(16);
+  const retangulo = new Retangulo(5, 3);
+  expect(retangulo.calcularPerimetro()).toBe(16);
 });
